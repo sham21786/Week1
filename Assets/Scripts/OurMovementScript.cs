faefba8ab56a0d73ea59a7dc24e8f7785ad3a3d1
Shazmil Nassar <sham21786@yahoo.com>
2015-05-08 13:52:59 +0100

    Week 1 Excerse
    moving block

---
+++ Assets/Scripts/OurMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OurMovementScript : MonoBehaviour {
+
+    private float speed = 12f;
+
+	void Update () {
+        MovePlayer();
+        RotatePlayer();
+	}
+
+    void MovePlayer() {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * speed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.down, Time.deltaTime * speed * 5   );
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * speed * 5);
+        }
+    }
+}
---
+++ Assets/Scripts/VideoTutorialMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoTutorialMovement : MonoBehaviour {
+
+    private float speed = 12f;
+
+	void Update () {
+        BoxMovement();
+        BoxRotation();
+	}
+
+    void BoxMovement() {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * speed;
+        }
+    }
+
+    void BoxRotation() {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down, Time.deltaTime * speed * speed);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * speed * speed);
+        }
+    }
+}
